927d8ccac2305fb8b5a8778a5f1063e0927f289a
Guilherme <guilhermef25@gmail.com>
2023-06-04 14:07:27 -0300

    Conectando ao DB

--- dbConnect.py
+++ dbConnect.py
@@ -1,2 +1,21 @@
-import pyodbc 
-cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for SQL Server};Server=localhost;Database=WhatsappSender-DEV;Port=1433;User ID=Guilherme\30cmmole;Password=1307')
+import pyodbc
+import os
+import json
+
+connStr = (
+    r'DSN=whatsmanager;'
+    r'Database=WhatsappSender-DEV;'
+    r'Trusted_Connection=yes;'
+    )
+
+cnxn = pyodbc.connect(connStr)
+
+cursor = cnxn.cursor()
+cursor.execute("INSERT INTO CLIENTES (id, nome, sobrenome, telefone, data_nascimento, ativo) VALUES (3, 'Erika', 'Pinheiro', '63984208152', '15/10/2002', 'S')") 
+cursor.execute('commit')
+
+cursor.execute("SELECT * FROM CLIENTES") 
+row = cursor.fetchone() 
+while row:
+    print (row) 
+    row = cursor.fetchone()
